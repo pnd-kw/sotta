f72ed256ea665fa91839801286902ba8b1af5e98
Pandu Office PC <pandu.usahaku@gmail.com>
2025-07-02 09:09:36 +0700

    fix: temporary shutdown gallery detail page

--- app/gallery/[id]/page.tsx
+++ app/gallery/[id]/page.tsx
@@ -1,69 +1,72 @@
-import { getGalleryImagesById } from "@/app/api/gallery/getGalleryImageById";
-import GalleryDetail from "@/app/components/GalleryDetail";
-import { notFound } from "next/navigation";
-import type { Metadata } from "next";
+// import { getGalleryImagesById } from "@/app/api/gallery/getGalleryImageById";
+// import GalleryDetail from "@/app/components/GalleryDetail";
+// import { notFound } from "next/navigation";
+// import type { Metadata } from "next";
 
-type Props = {
-  params: { id: string };
-  searchParams?: { preview?: string };
-};
+// type Props = {
+//   params: { id: string };
+//   searchParams?: { preview?: string };
+// };
 
-export async function generateMetadata({
-  params,
-}: Props): Promise<Metadata | null> {
-  try {
-    const image = await getGalleryImagesById({ id: params.id });
+// export async function generateMetadata({
+//   params,
+// }: Props): Promise<Metadata | null> {
+//   try {
+//     const image = await getGalleryImagesById({ id: params.id });
 
-    if (!image || !image.published) return null;
+//     if (!image || !image.published) return null;
 
-    return {
-      title: image.name,
-      description: image.caption,
-      openGraph: {
-        title: image.name,
-        description: image.caption,
-        images: [
-          {
-            url: `${image.imageUrl}`,
-            width: 800,
-            height: 600,
-            alt: image.alt,
-          },
-        ],
-      },
-      twitter: {
-        card: "summary_large_image",
-        title: image.name,
-        description: image.caption,
-        images: [`${image.imageUrl}`],
-      },
-    };
-  } catch (error) {
-    console.log("Failed to get metadata", error);
-    return {};
-  }
+//     return {
+//       title: image.name,
+//       description: image.caption,
+//       openGraph: {
+//         title: image.name,
+//         description: image.caption,
+//         images: [
+//           {
+//             url: `${image.imageUrl}`,
+//             width: 800,
+//             height: 600,
+//             alt: image.alt,
+//           },
+//         ],
+//       },
+//       twitter: {
+//         card: "summary_large_image",
+//         title: image.name,
+//         description: image.caption,
+//         images: [`${image.imageUrl}`],
+//       },
+//     };
+//   } catch (error) {
+//     console.log("Failed to get metadata", error);
+//     return {};
+//   }
+// }
+
+// export default async function GalleryDetailPage({
+//   params,
+//   searchParams,
+// }: Props) {
+//   const allowPreview = searchParams?.preview?.toLowerCase() === "true";
+
+//   try {
+//     const image = await getGalleryImagesById({ id: params.id });
+
+//     if (!image || (!image.published && !allowPreview)) return notFound();
+
+//     const transformPublishedType = {
+//       ...image,
+//       published: Boolean(image.published),
+//     };
+
+//     return <GalleryDetail data={transformPublishedType} />;
+//   } catch (error) {
+//     console.error("Failed to fetch gallery image", error);
+//     return notFound();
+//   }
+// }
+
+export default function GalleryDetailPage() {
+  return <div>Under construction</div>;
 }
-
-export default async function GalleryDetailPage({
-  params,
-  searchParams,
-}: Props) {
-  const allowPreview = searchParams?.preview?.toLowerCase() === "true";
-
-  try {
-    const image = await getGalleryImagesById({ id: params.id });
-
-    if (!image || (!image.published && !allowPreview)) return notFound();
-
-    const transformPublishedType = {
-      ...image,
-      published: Boolean(image.published),
-    };
-
-    return <GalleryDetail data={transformPublishedType} />;
-  } catch (error) {
-    console.error("Failed to fetch gallery image", error);
-    return notFound();
-  }
-}
-
